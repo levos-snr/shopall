d4d90ea36cd43502de49c676174f6072cccc9d49
iyiola olusola ebenezer <sholaiyiola1966@gmail.com>
2024-08-14 18:58:59 +1000

    "Added two empty lines to HeroSection component new"

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,30 @@
+import FetchProducts from "../lib/FetchProducts";
+import "./HeroSection.css"
+
+
 const HeroSection = () => {
+  const { products, loading } = FetchProducts();
+  console.log(`${products}`)
+  const picture = products.map(pic => pic.thumbnail)
+  console.log(picture)
+  const slicedPic = picture.slice(0, 11);
   return (
+
+    
     <div className="hero-section">
-      <h1>Hero Section</h1>
+      <h1></h1>
+      <div className="flex">
+      {
+        slicedPic.map(p =>
+          <img src={p} alt="product" />
+        )
+      }
+      </div>
+     
     </div>
   );
 };
 
 export default HeroSection;
+
 
